1f38a4f06cb05c8732ce7061a6139f08c0ba41b6
Maksym Matyjaśkiewicz <89651867+maxiohaze@users.noreply.github.com>
2026-06-25 15:55:06 +0200

    social: add jwt authentication integration

---
+++ SocialService/SocialService.Api/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialService.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Test()
+    {
+        var userId = User.FindFirst("userId")?.Value;
+        var username = User.FindFirst("username")?.Value;
+
+        return Ok(new
+        {
+            userId,
+            username
+        });
+    }
+}
--- SocialService/SocialService.Api/Program.cs
+++ SocialService/SocialService.Api/Program.cs
@@ -1,3 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SocialService.Application.Security;
+
 namespace SocialService.Api;
 
 public class Program
@@ -6,13 +11,62 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         
+        builder.Services.AddControllers();
         builder.Services.AddAuthorization();
         
+        // Configure JWT settings from appsettings.json
+        builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+
+        var jwtSettings = builder.Configuration
+                              .GetSection("Jwt")
+                              .Get<JwtSettings>() ?? throw new InvalidOperationException("JWT settings are missing.");
+        
+        // Configure JWT authentication
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var token = context.Request.Cookies["jwt"];
+
+                        if (!string.IsNullOrEmpty(token))
+                        {
+                            context.Token = token;
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key)),
+
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        
+        // Build the application
         var app = builder.Build();
         
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
+        
+        app.MapControllers();
         
         app.Run();
     }
