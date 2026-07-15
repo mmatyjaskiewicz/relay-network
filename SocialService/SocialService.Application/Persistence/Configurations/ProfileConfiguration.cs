cd9ad23719c41310a34a81db39926075be10e097
Maksym Matyjaśkiewicz <89651867+maxiohaze@users.noreply.github.com>
2026-07-15 11:56:22 +0200

    add env variables for rabbitmq in docker-compose and create profile config for ef

---
+++ SocialService/SocialService.Application/Persistence/Configurations/ProfileConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialService.Application.Entities;
+
+namespace SocialService.Application.Persistence.Configurations;
+
+public class ProfileConfiguration : IEntityTypeConfiguration<ProfileEntity>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<ProfileEntity> builder)
+    {
+        builder.ToTable("profiles");
+        
+        builder.HasKey(fr => fr.Id);
+        builder.Property(fr => fr.Id).HasColumnName("id");
+        builder.Property(fr => fr.UserId).HasColumnName("user_id").IsRequired();
+        builder.Property(fr => fr.Username).HasColumnName("username").IsRequired();
+    }
+}
