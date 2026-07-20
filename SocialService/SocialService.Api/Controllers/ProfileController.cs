54137d9123a56bba8e4777eba279618176a06ff3
Maksym Matyjaśkiewicz <89651867+maxiohaze@users.noreply.github.com>
2026-07-20 20:54:23 +0200

    social: add initial profile controller

---
+++ SocialService/SocialService.Api/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialService.Application.DTOs.Requests;
+using SocialService.Application.Interfaces;
+
+namespace SocialService.Api.Controllers;
+
+[ApiController]
+[Route("api/profile")]
+public class ProfileController(IAvatarStorage avatarStorage) : ControllerBase
+{
+    [HttpPost("avatar")]
+    public async Task<IActionResult> UploadAvatar(IFormFile file, CancellationToken cancellationToken)
+    {
+        var objectName = await avatarStorage.UploadAsync(new UploadAvatarRequest(file.OpenReadStream(), file.FileName, file.ContentType, file.Length), cancellationToken);
+
+        return Ok(new
+        {
+            ObjectName = objectName
+        });
+    }
+}
