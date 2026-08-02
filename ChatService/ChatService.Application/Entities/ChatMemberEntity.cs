33063ea4f19b9e7174d86a42225fc6896b9bfa43
Maksym Matyjaśkiewicz <89651867+maxiohaze@users.noreply.github.com>
2026-08-02 11:05:41 +0200

    chat: create initial chat service

--- ChatService/ChatService.Application/Entities/ChatMemberEntity.cs
+++ ChatService/ChatService.Application/Entities/ChatMemberEntity.cs
@@ -4,6 +4,7 @@ public class ChatMemberEntity
 {
     public Guid Id { get; set; }
     public Guid ChatId { get; set; }
+    public Guid UserId { get; set; }
 
     public ChatEntity? Chat { get; set; } = null;
 }
--- ChatService/ChatService.Application/Services/ChatService.cs
+++ ChatService/ChatService.Application/Services/ChatService.cs
@@ -1,6 +1,31 @@
-﻿namespace ChatService.Application.Services;
+﻿using ChatService.Application.Entities;
+using ChatService.Application.Enums;
+using ChatService.Application.Interfaces;
 
-public class ChatService
+namespace ChatService.Application.Services;
+
+public class ChatService(IChatRepository chatRepository)
 {
-    
+    public async Task CreatePrivateChatAsync(Guid userId, Guid friendId)
+    {
+        var chat = new ChatEntity
+        {
+            Id = Guid.NewGuid(),
+            Type = ChatType.Direct
+        };
+
+        await chatRepository.CreateChatAsync(chat);
+
+        await chatRepository.AddChatMemberAsync(new ChatMemberEntity
+        {
+            ChatId = chat.Id,
+            UserId = userId
+        });
+
+        await chatRepository.AddChatMemberAsync(new ChatMemberEntity
+        {
+            ChatId = chat.Id,
+            UserId = friendId
+        });
+    }
 }
